da2d1217875bce7f59bd1fe889fd2e14090e9d13
vipul <vipul2497@gmail.com>
2020-06-14 02:23:25 +0530

    Update theindividual.php

--- theindividual.php
+++ theindividual.php
@@ -11,16 +11,29 @@ $x=33;
 
 ?>
 <body>
+<script></script>
+<div class="backdrop"></div>
+    <div class="modal">
+        <h1 class="modal__title">Enter the Admin Credentials</h1>
+        <div class="modal__actions">
+          <form method="post" class="form">
+            <input type="email" placeholder="enter the email id" class="email" name="email" required email><br>
+            <input type="password" placeholder="enter password" class="pass" name="pass" required><br><br>
+            <button type="submit" name="submit" class="modal__action">LOGIN!</button>
+            <button class="modal__action modal__action--negative" type="button">No!</button>
+          </form>
+        </div>
+    </div>
 <header class="main-header">
         <div>
-            <a href="https://localhost/initialtion.php" class="main-header__brand">
-                The World Is Here
+            <a href="https://localhost/theindividual.php" class="main-header__brand">
+                The Individual
             </a>
         </div>
         <nav class="main-nav">
             <ul class="main-nav__items">
                 <li class= "main-nav__item main-header__items--cta">
-                    <a href="https://localhost/admin.php" class="main-header__items">Admin</a>
+                    <a  class="main-header__items">Admin</a>
                 </li>
             </ul>
         </nav>
@@ -79,14 +92,15 @@ $x=33;
             <li>Sports</li>
           </ul>
       </div>
-      <div class="me">
-          <h2>About ME</h2>
-          <ul>
-            <li><img src="https://drive.google.com/uc?id=1QxsDz3RX6hlU18zCjOD3ueharENa8NJB" class="image"></li>
-          </ul>
+      <div class="meetme">
+        <ul>
+        <li><img src="https://drive.google.com/uc?id=1QxsDz3RX6hlU18zCjOD3ueharENa8NJB"  class="image"></li>
+        <li class="mine"><br><br><br>Click here to know more about the author of the Blog</li>
+        </ul>
       </div>
     </section>
   </section>
+  </main>
   <footer class="main-footer">
         <nav>
             <ul class="main-footer__links">
@@ -99,8 +113,6 @@ $x=33;
             </ul>
         </nav>
   </footer>
-  </main>
+  <script src="shared.js"></script>
 </body>
-<script>
-</script>
 </html>
